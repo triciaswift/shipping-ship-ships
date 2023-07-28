6540acea68a5cddb498f681a20249e507537c9ab
Tricia Swift <tricia.swift8@gmail.com>
2023-07-28 11:01:40 -0500

    created the ship count for haulers and tweaked code for the event listener on shipList file

--- scripts/ShipList.js
+++ scripts/ShipList.js
@@ -1,30 +1,15 @@
 import { getShippingShips, getHaulers } from "./database.js";
 
-const ships = getShippingShips();
-
-document.addEventListener("click", (clickEvent) => {
-  const itemClicked = clickEvent.target;
-
-  const shipName = itemClicked.dataset.shipname;
-  const haulerId = itemClicked.dataset.haulerforeignkey;
-  const haulers = getHaulers();
-  let haulingShip = { name: "Incorrect" };
-
-  for (const hauler of haulers) {
-    if (hauler.id === parseInt(haulerId)) {
-      haulingShip = hauler.name;
-      window.alert(`${shipName} is being hauled by ${haulingShip}`);
-    }
-  }
-});
-
 export const ShipList = () => {
+  const ships = getShippingShips();
+
   let shipsHTML = `<ul class="list--style">`;
 
   for (const ship of ships) {
     shipsHTML += `<li class="list--background"
                       data-shipName="${ship.name}"
-                      data-haulerForeignKey="${ship.haulerId}"
+                      data-haulerId="${ship.haulerId}"
+                      data-type="ship"
                       >${ship.name}</li>`;
   }
 
@@ -32,3 +17,29 @@ export const ShipList = () => {
 
   return shipsHTML;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  // Was a shipping ship list item clicked?
+  if (itemClicked.dataset.type === "ship") {
+    // Get the haulerId value of the shipping ship clicked
+    const haulerId = itemClicked.dataset.haulerid;
+    const shipName = itemClicked.dataset.shipname;
+    // Define a default object for the found hauler
+    let haulingShip = { name: "Incorrect" };
+
+    // Iterate the array of hauler objects
+    const haulers = getHaulers();
+    for (const hauler of haulers) {
+      // Does the haulerId foreign key match the id of the current hauler?
+      if (hauler.id === parseInt(haulerId)) {
+        // Reassign the value of `haulingShip` to the current hauler
+        haulingShip = hauler.name;
+      }
+    }
+    // Show an alert to the user with this format...
+    window.alert(`${shipName} is being hauled by ${haulingShip}`);
+    // Palais Royal is being hauled by Seawise Giant
+  }
+});
