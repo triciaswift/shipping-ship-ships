35e768fdc62c527503d8e214c6d9fb1e2c60596e
Tricia Swift <tricia.swift8@gmail.com>
2023-07-28 11:31:29 -0500

    added event listener for docks and which haulers they were unloading.

--- scripts/DockList.js
+++ scripts/DockList.js
@@ -1,4 +1,4 @@
-import { getDocks } from "./database.js";
+import { getDocks, getHaulers } from "./database.js";
 
 export const DockList = () => {
   const docks = getDocks();
@@ -6,7 +6,11 @@ export const DockList = () => {
   let docksHTML = `<ul class="list--style">`;
 
   for (const dock of docks) {
-    docksHTML += `<li class="list--background">
+    docksHTML += `<li class="list--background"
+                      data-type="dock"
+                      data-id="${dock.id}"
+                      data-location="${dock.location}"
+    >
     ${dock.location} can hold ${dock.volume} million tons of cargo
     </li>`;
   }
@@ -15,3 +19,22 @@ export const DockList = () => {
 
   return docksHTML;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  if (itemClicked.dataset.type === "dock") {
+    const dockId = itemClicked.dataset.id;
+    const dockLocation = itemClicked.dataset.location;
+
+    let haulerName = "nothing";
+
+    const haulers = getHaulers();
+    for (const hauler of haulers) {
+      if (parseInt(dockId) === hauler.dockId) {
+        haulerName = hauler.name;
+      }
+    }
+    window.alert(`The ${dockLocation} is currently unloading ${haulerName}`);
+  }
+});
